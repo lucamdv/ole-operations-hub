35d6b678750de769700c261c0e96989209ee1cf7
Luca Monteiro <163064720+lucamdv@users.noreply.github.com>
2026-08-25 14:38:15 -0300

    Separa lint semântico da formatação Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -36,5 +36,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
-  eslintPluginPrettier,
+  // Formatação é validada separadamente pelo Prettier; o ESLint fica focado
+  // em problemas semânticos e de qualidade de código.
+  eslintConfigPrettier,
 );
